1130bcde332d3a8c8ba892ac8d93023f644d29d1
TechGroup2020 <65551463+TechGroup2020@users.noreply.github.com>
2021-01-09 19:42:03 +0300

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -2,7 +2,7 @@
 docker stop Frontendcont       
 docker rm Frontendcont          
 docker build -t frontendimg .
-docker run -i --name Frontendcont --network=contnetwork --ip 192.168.10.4  -p 5050:5050  frontendimg
+docker run -i --name Frontendcont --network=contnetwork --ip 192.168.10.4  -p 5050:5050  frontendimg /bin/bash
 docker run -it --name Frontendcont frontendimg /bin/bash
 docker start Frontendcont
 docker exec Frontendcont ./startup.sh
